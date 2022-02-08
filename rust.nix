cceb78d3bd15f0ec5f3692a49583960de14cfa1c
Matthias Beyer <mail@beyermatthias.de>
2022-02-08 16:32:32 +0100

    Refactor building of crossPkgs attribute into helper function
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- rust.nix
+++ rust.nix
@@ -10,17 +10,18 @@ let
 
   pkgs = import <nixpkgs-unstable> { overlays = [ moz_overlay ]; };
 
+  mkCrossPkgs = triple: libc: (import <nixpkgs> {
+    overlays = [ moz_overlay ];
+    crossSystem = {
+      inherit libc;
+      config = triple;
+      rustc.config = triple;
+    };
+  });
+
   targets = {
     aarch64-unknown-linux-gnu = let
-      crossPkgs = import <nixpkgs> {
-        overlays = [ moz_overlay ];
-        crossSystem = {
-          config = "aarch64-unknown-linux-gnu";
-          libc = "glibc";
-
-          rustc.config = "aarch64-unknown-linux-gnu";
-        };
-      };
+      crossPkgs = mkCrossPkgs "aarch64-unknown-linux-gnu" "glibc";
     in {
       rustPackages = [
         (pkgs.rustChannels.stable.rust.override { targets = [ "aarch64-unknown-linux-gnu" ]; })
@@ -32,15 +33,7 @@ let
     };
 
     aarch64-unknown-linux-musl = let
-      crossPkgs = import <nixpkgs> {
-        overlays = [ moz_overlay ];
-        crossSystem = {
-          config = "aarch64-unknown-linux-musl";
-          libc = "musl";
-
-          rustc.config = "aarch64-unknown-linux-musl";
-        };
-      };
+      crossPkgs = mkCrossPkgs "aarch64-unknown-linux-musl" "musl";
     in {
       rustPackages = [
         (pkgs.rustChannels.stable.rust.override { targets = [ "aarch64-unknown-linux-musl" ]; })
@@ -53,15 +46,7 @@ let
     };
 
     arm-unknown-linux-gnueabihf = let
-      crossPkgs = import <nixpkgs> {
-        overlays = [ moz_overlay ];
-        crossSystem = {
-          config = "arm-unknown-linux-gnueabihf";
-          libc = "glibc";
-
-          rustc.config = "arm-unknown-linux-gnueabihf";
-        };
-      };
+      crossPkgs = mkCrossPkgs "arm-unknown-linux-gnueabihf" "glibc";
     in {
       rustPackages = [
         (pkgs.rustChannels.stable.rust.override { targets = [ "arm-unknown-linux-gnueabihf" ]; })
@@ -73,15 +58,7 @@ let
     };
 
     armv7-unknown-linux-gnu = let
-      crossPkgs = import <nixpkgs> {
-        overlays = [ moz_overlay ];
-        crossSystem = {
-          config = "armv7l-unknown-linux-gnueabihf";
-          libc = "glibc";
-
-          rustc.config = "armv7-unknown-linux-gnueabihf";
-        };
-      };
+      crossPkgs = mkCrossPkgs "armv7l-unknown-linux-gnueabihf" "glibc";
     in {
       rustPackages = [
         (pkgs.rustChannels.stable.rust.override { targets = [ "armv7-unknown-linux-gnueabihf" ]; })
@@ -95,15 +72,7 @@ let
     };
 
     armv7-unknown-linux-musleabihf = let
-      crossPkgs = import <nixpkgs> {
-        overlays = [ moz_overlay ];
-        crossSystem = {
-          config = "armv7l-unknown-linux-musleabihf";
-          libc = "musl";
-
-          rustc.config = "armv7l-unknown-linux-musleabihf";
-        };
-      };
+      crossPkgs = mkCrossPkgs "armv7l-unknown-linux-musleabihf" "musl";
     in {
       rustPackages = [
         (pkgs.rustChannels.stable.rust.override { targets = [ "armv7-unknown-linux-musleabihf" ]; })
