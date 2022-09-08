1d450de209d3e42a475f753692b843f0cb4fca4d
Matthias Beyer <mail@beyermatthias.de>
2022-09-08 09:10:33 +0200

    Add rust-expressiv.nix
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

---
+++ rust-espressiv.nix
@@ -0,0 +1,62 @@
+{ ... }:
+
+let
+  moz_overlay = import (
+    builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz
+  );
+
+  pkgs = import <nixpkgs-unstable> { overlays = [ moz_overlay ]; };
+
+  cargo-espflash = pkgs.callPackage ./cargo-espflash.nix { };
+  ldproxy = pkgs.callPackage ./ldproxy.nix { };
+
+  channel = pkgs.rustChannelOf {
+    channel = "nightly";
+    date = "2022-03-10";
+  };
+
+  targetPackages = with channel; [
+    rust-std
+    rust-src
+    (rust.override { extensions = ["rust-src" ]; })
+    rustc
+    cargo
+  ];
+
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    cargo-audit
+    cargo-bloat
+    cargo-deny
+    cargo-depgraph
+    cargo-deps
+    cargo-diet
+    cargo-flamegraph
+    cargo-geiger
+    cargo-graph
+    cargo-license
+    cargo-modules
+    cargo-outdated
+    cargo-llvm-lines
+
+    cargo-espflash
+    ldproxy
+
+    llvm
+    libclang
+    clang
+    openssl
+    pkg-config
+  ];
+
+  shellHook = ''
+    export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+
+    echo " rustc: ''$(rustc --version)"
+    echo " cargo: ''$(cargo --version)"
+    echo "... have fun!"
+  '';
+
+  LIBCLANG_PATH   = "${pkgs.llvmPackages.libclang}/lib";
+}
